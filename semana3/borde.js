713f69f23816a6d2f9b608905d1b830126c06f72
JOSE BETANCOURTH <JOSE@MacBook-Air-de-JOSE.local>
2022-09-15 10:15:58 -0500

    funcion borde

--- semana3/borde.js
+++ semana3/borde.js
@@ -1,13 +1,13 @@
 const tabla=document.getElementsByTagName("table");
-const fila=document.getElementsByTagName("tr"); //arreglo de elementos tr
-const celda=document.getElementsByTagName("td");// arreglo d elemento td
+const fila=document.getElementsByTagName("tr"); 
+const celda=document.getElementsByTagName("td");
 //tipo es "2px dotted blue" o "2px dotted cornsilk"
 function borde(tipo){
 tabla.item(0).style.border=tipo;
 
-for(let i=0;i<fila.length;i++)//recorre cada elemento fila y asigna propiedad
- fila.item(i).style.border=tipo;
-
-for(let i=0;i<celda.length;i++)//recorre cada elemento celda y asigna propiedad
- celda.item(i).style.border=tipo;   
+for(i in fila)
+ {//recorre cada elemento fila y asigna propiedad
+  fila.item(i).style.border=tipo;
+  celda.item(i).style.border=tipo;                            
+ }
 }
